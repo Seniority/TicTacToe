64a6744485d53a055b71490ba229b68211374102
Seniority <senioritylabs@gmail.com>
2016-01-31 02:02:29 -0800

    first commit for Tic Tac Toe

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Classic game of Tick Tack Toe by Seniority Labs.
+    /// Coded by Anthony Senior II, 11/19/2015.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        bool turn = true; // True is X's turn. False is O's turn.
+        int turn_count = 0; // Keeps track of how many turns have occurred.
+        bool haveWinner = false; // If haveWinner is true, that means there is a winner. False means there is no winner yet.
+        static String player1, player2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /******************************* Player Names Form ************************************/
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+
+            label1.Text = player1 + " (X)";
+            label3.Text = player2 + " (O)";
+        }
+
+        public static void SetPlayerNames(String name1, String name2)
+        {
+            player1 = name1;
+            player2 = name2;            
+        }
+
+
+        /********************************** Menu Strip ****************************************/
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e) // File>New Game
+        {
+            turn = true;
+            turn_count = 0;
+            haveWinner = false;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                    b.BackColor = SystemColors.Control;
+                }
+                catch { }
+            }
+
+            newGameButton.Text = "New Game";
+        }
+
+        private void changePlayersToolStripMenuItem_Click(object sender, EventArgs e) // File>Change Players
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+
+            label1.Text = player1 + "(X)";
+            label3.Text = player2 + "(O)";
+            o_win_count.Text = "0";
+            x_win_count.Text = "0";
+            draw_count.Text = "0";
+            turn = true;
+            turn_count = 0;
+            haveWinner = false;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                    b.BackColor = SystemColors.Control;
+                }
+                catch { }
+            }
+
+            newGameButton.Text = "New Game";
+        }
+
+        private void resetScoreboardToolStripMenuItem_Click(object sender, EventArgs e) // File>Reset Scoreboard
+        {
+            o_win_count.Text = "0";
+            x_win_count.Text = "0";
+            draw_count.Text = "0";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) // File>Exit
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) // Help>About
+        {
+            string about_message = "";
+            about_message += "Welcome to Tic Tac Toe by Seniority Labs\n\n";
+            about_message += "This is the classic game of Tic Tac Toe in wich two\n";
+            about_message += "players take turns marking either \"X\" or \"O\" on\n";
+            about_message += "the game's boxes. The first player to make a line of \n";
+            about_message += "three X's or O's will win the game. The line of marks \n";
+            about_message += "may be vertical, horizontal, or diagonal. If neither \n";
+            about_message += "player is able to make a line of three marks before all\n";
+            about_message += "of the boxes are filled, the game will be a draw and \n";
+            about_message += "nobody wins the game. Have fun!";            
+
+            MessageBox.Show(about_message, "About Tic Tac Toe");
+        }
+
+
+        /*************************************** Buttons ****************************************/
+        private void newGameButton_Click(object sender, EventArgs e) //New Game button
+        {
+            turn = true;
+            turn_count = 0;
+            haveWinner = false;
+
+                foreach (Control c in Controls)
+                {
+                    try {
+                        Button b = (Button)c;
+                        b.Enabled = true;
+                        b.Text = "";
+                        b.BackColor = SystemColors.Control;
+                    }
+                    catch { }
+                }   
+                        
+            newGameButton.Text = "New Game";
+        }
+
+        private void button_click(object sender, EventArgs e) // Game buttons for the X and O marks
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            b.Enabled = false; // Disables the button after it is clicked
+            turn = !turn; // Switches turn. If the last button text said "X" then the next one will say "O".
+            turn_count++;
+            checkForWinner();
+        }
+
+        private void checkForWinner() // Checks to see if there is a line of three matching marks (a winner)
+        {
+            // Horizontal mark checks
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                haveWinner = true;
+                A1.BackColor = SystemColors.ActiveCaption;
+                A2.BackColor = SystemColors.ActiveCaption;
+                A3.BackColor = SystemColors.ActiveCaption;
+            }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                haveWinner = true;
+                B1.BackColor = SystemColors.ActiveCaption;
+                B2.BackColor = SystemColors.ActiveCaption;
+                B3.BackColor = SystemColors.ActiveCaption;
+            }
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                haveWinner = true;
+                C1.BackColor = SystemColors.ActiveCaption;
+                C2.BackColor = SystemColors.ActiveCaption;
+                C3.BackColor = SystemColors.ActiveCaption;
+            }
+
+            // Vertical mark checks
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                haveWinner = true;
+                A1.BackColor = SystemColors.ActiveCaption;
+                B1.BackColor = SystemColors.ActiveCaption;
+                C1.BackColor = SystemColors.ActiveCaption;
+            }
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                haveWinner = true;
+                A2.BackColor = SystemColors.ActiveCaption;
+                B2.BackColor = SystemColors.ActiveCaption;
+                C2.BackColor = SystemColors.ActiveCaption;
+            }
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                haveWinner = true;
+                A3.BackColor = SystemColors.ActiveCaption;
+                B3.BackColor = SystemColors.ActiveCaption;
+                C3.BackColor = SystemColors.ActiveCaption;
+            }
+
+            // Diagonal mark checks
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                haveWinner = true;
+                A1.BackColor = SystemColors.ActiveCaption;
+                B2.BackColor = SystemColors.ActiveCaption;
+                C3.BackColor = SystemColors.ActiveCaption;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+            {
+                haveWinner = true;
+                A3.BackColor = SystemColors.ActiveCaption;
+                B2.BackColor = SystemColors.ActiveCaption;
+                C1.BackColor = SystemColors.ActiveCaption;
+            }
+
+            if (haveWinner) // Execute this code if there is a winner
+            {
+                disableButtons();
+                string winner = "";
+                if (turn)
+                {
+                    winner = player2;
+                    o_win_count.Text = (Int32.Parse(o_win_count.Text) + 1).ToString();
+                }
+                else
+                {
+                    winner = player1;
+                    x_win_count.Text = (Int32.Parse(x_win_count.Text) + 1).ToString();
+                }
+                MessageBox.Show(winner + " wins!", "WINNER!");
+            }
+            else // Execute this code if all boxes were filled with marks, but no winner was found
+            {
+                if (turn_count == 9)
+                {
+                    draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
+                    MessageBox.Show("The game was a draw!", "Draw");
+                }
+            }
+        }
+
+        private void disableButtons() // Disable the buttons after a game is won or draw
+        {
+                foreach (Control c in Controls)
+                {
+                    try
+                    {
+                        Button b = (Button)c;
+                        b.Enabled = false;
+                    }
+                    catch { }                    
+                }
+            
+            newGameButton.Enabled = true; // Re-enable the New Game button
+        }            
+
+        private void button_enter(object sender, EventArgs e) //Display's the player's mark if the mouse is hovering over the button
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                {
+                    b.Text = "X";
+                }
+                else
+                {
+                    b.Text = "O";
+                }
+            }
+        }
+
+        private void button_leave(object sender, EventArgs e) //Clear's the player's mark once the mouse leaves the button
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                {
+                    b.Text = "";
+                }
+                else
+                {
+                    b.Text = "";
+                }
+            }
+        }
+
+        ToolTip tt_newGameButton = new ToolTip();
+        private void newGameButton_MouseHover(object sender, EventArgs e)
+        {
+            tt_newGameButton.Show("Begins a new game", newGameButton);
+        }
+
+        
+    }// end class
+}// end namespace
---
+++ TicTacToe/Form2.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TicTacToe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerOneLabel = new System.Windows.Forms.Label();
+            this.playerTwoLabel = new System.Windows.Forms.Label();
+            this.playerOne = new System.Windows.Forms.TextBox();
+            this.playerTwo = new System.Windows.Forms.TextBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playerOneLabel
+            // 
+            this.playerOneLabel.AutoSize = true;
+            this.playerOneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerOneLabel.Location = new System.Drawing.Point(11, 73);
+            this.playerOneLabel.Name = "playerOneLabel";
+            this.playerOneLabel.Size = new System.Drawing.Size(119, 20);
+            this.playerOneLabel.TabIndex = 0;
+            this.playerOneLabel.Text = "Player One (X): ";
+            // 
+            // playerTwoLabel
+            // 
+            this.playerTwoLabel.AutoSize = true;
+            this.playerTwoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerTwoLabel.Location = new System.Drawing.Point(11, 109);
+            this.playerTwoLabel.Name = "playerTwoLabel";
+            this.playerTwoLabel.Size = new System.Drawing.Size(119, 20);
+            this.playerTwoLabel.TabIndex = 1;
+            this.playerTwoLabel.Text = "Player Two (O): ";
+            // 
+            // playerOne
+            // 
+            this.playerOne.Location = new System.Drawing.Point(122, 75);
+            this.playerOne.Name = "playerOne";
+            this.playerOne.Size = new System.Drawing.Size(189, 20);
+            this.playerOne.TabIndex = 2;
+            this.playerOne.Text = "Player 1";
+            // 
+            // playerTwo
+            // 
+            this.playerTwo.Location = new System.Drawing.Point(122, 111);
+            this.playerTwo.Name = "playerTwo";
+            this.playerTwo.Size = new System.Drawing.Size(189, 20);
+            this.playerTwo.TabIndex = 3;
+            this.playerTwo.Text = "Computer";
+            this.playerTwo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.playerTwo_KeyPress);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(236, 137);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 4;
+            this.playButton.Text = "Play!";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.Location = new System.Drawing.Point(12, 9);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(299, 53);
+            this.welcomeLabel.TabIndex = 5;
+            this.welcomeLabel.Text = "Type the names of the players below. If you would like to play against the Comput" +
+    "er, leave player two as \"Computer\". Otherwise, enter the name of the second huma" +
+    "n player.";
+            this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 168);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.playerTwo);
+            this.Controls.Add(this.playerOne);
+            this.Controls.Add(this.playerTwoLabel);
+            this.Controls.Add(this.playerOneLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label playerOneLabel;
+        private System.Windows.Forms.Label playerTwoLabel;
+        private System.Windows.Forms.TextBox playerOne;
+        private System.Windows.Forms.TextBox playerTwo;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label welcomeLabel;
+    }
+}
